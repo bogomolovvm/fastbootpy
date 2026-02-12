1ac896ff251b84d9973c33cfb9a7e6021c201581
Vladislav Bogomolov <vladislav.bogomolov@eltex.loc>
2026-02-12 10:30:19 +0700

    tests for usb_layer

--- tests/transport/test_transport.py
+++ tests/transport/test_transport.py
@@ -5,7 +5,7 @@ import pytest
 from fastbootpy.transport.usb_transport import UsbTransport
 
 
-class TestTransport:
+class TestUSBTransportSend:
     def test_send_write_to_endpoint(self):
         # Arrange
         mock_write_ep = MagicMock()
@@ -22,3 +22,23 @@ class TestTransport:
 
         # Assert
         mock_write_ep.write.assert_called_once_with(b"getvar:all", 100)
+
+class TestUSBTransportRecv:
+    def test_recv_w_size_and_timeout_from_endpoint(self):
+        # Arrange
+        mock_read_ep = MagicMock()
+        timeout = 100
+        transport = UsbTransport(
+            serial="ABC",
+            usb_device=MagicMock(),
+            read_endpoint=mock_read_ep,
+            write_endpoint=MagicMock(),
+            read_timeout=timeout
+        )
+        size = 128
+
+        # Act
+        package = transport.receive(size=size)
+
+        # Assert
+        mock_read_ep.read.assert_called_once_with(size, timeout)
